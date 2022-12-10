eda524230453e6893aa33167cbb4fc64d447c119
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-10 11:00:34 +0000

    Adding tarfile member sanitization to extractall()

--- src/workstation/os_manager.py
+++ src/workstation/os_manager.py
@@ -68,7 +68,26 @@ class Fedora(OSManager):
         os.makedirs(dest, exist_ok=True)
         file = wget.download(url)
         with tarfile.open(file, "r:gz") as tar:
-            tar.extractall()
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar)
             folder = tar.getnames()[0]
         shutil.copy(os.path.abspath(os.path.join(folder, binary_name)), dest)
         Path(os.path.abspath(file)).unlink(missing_ok=True)
